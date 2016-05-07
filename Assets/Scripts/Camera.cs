66cb67f9695558c16410ed7c1d357a73452e9293
Mana234 <manaman234@gmail.com>
2016-05-07 19:24:21 -0400

    Added Dragon PlaceHolder and code

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+
+	private Vector3 moveDirection = Vector3.zero;
+	public float speed = 30.0f;
+
+	void Start() {
+	}
+
+
+	void Update()
+	{
+		CharacterController controller = GetComponent<CharacterController>();
+		
+		moveDirection = transform.TransformDirection(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		moveDirection *= speed;
+		
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+
+
+}
