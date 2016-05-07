f5a8ce4a15c248e3bf728dc57e06f6fabb545dc2
Mana234 <manaman234@gmail.com>
2016-05-07 16:44:28 -0400

    Added Sound Effects to the "Start Game" Button

--- Assets/Scripts/StuffForTitleScreen.cs
+++ Assets/Scripts/StuffForTitleScreen.cs
@@ -2,13 +2,26 @@
 using System.Collections;
 
 public class StuffForTitleScreen : MonoBehaviour {
+
 	public void Button(int index) 
 	{
-		Application.LoadLevel(index);
+		StartCoroutine (launchAfterDelay (index));
 	}
 	
 	public void Button(string levelName)
 	{
-		Application.LoadLevel(levelName);
+		StartCoroutine (launchAfterDelay (levelName));
+	}
+
+	public IEnumerator launchAfterDelay(string levelName)
+	{
+		yield return new WaitForSeconds(1);
+		Application.LoadLevel (levelName);
+	}
+
+	public IEnumerator launchAfterDelay(int index)
+	{
+		yield return new WaitForSeconds(1);
+		Application.LoadLevel (index);
 	}
 }
