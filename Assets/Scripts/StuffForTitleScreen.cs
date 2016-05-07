a7c73a4bec24448497175fbed0373022763bb83e
Mana234 <manaman234@gmail.com>
2016-05-07 15:16:10 -0400

    Added Some Basic "Music Managers"

---
+++ Assets/Scripts/StuffForTitleScreen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StuffForTitleScreen : MonoBehaviour {
+	public void Button(int index) 
+	{
+		Application.LoadLevel(index);
+	}
+	
+	public void Button(string levelName)
+	{
+		Application.LoadLevel(levelName);
+	}
+}
