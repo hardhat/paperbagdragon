0ebe0a135448945f3e1241170135b37e62456787
Mana234 <manaman234@gmail.com>
2016-05-08 16:14:00 -0400

    Added Fire (Its Purple For Some Reason)
    Please someone fix the purple thing

---
+++ Assets/Scripts/FireBreathScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class FireBreathScript : MonoBehaviour {
+
+	public Rigidbody Torch;
+	public float speed = 10f;
+	
+	private void FireTorch () {
+		Rigidbody TorchClone = (Rigidbody) Instantiate(Torch, transform.position, transform.rotation);
+		TorchClone.velocity = transform.forward * speed;
+		
+		// You can also acccess other components / scripts of the clone
+		///TorchClone.GetComponent<MyRocketScript>().DoSomething();
+	}
+
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+			FireTorch();
+		}
+	}
+}
+
+
+	
