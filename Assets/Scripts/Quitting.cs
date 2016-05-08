a65f8a85a970752f4d32375411d29a90bd84c01f
Dale Wick <dmwick@yahoo.com>
2016-05-08 19:03:35 -0400

    Added quit ability.

---
+++ Assets/Scripts/Quitting.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quitting : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+
+    }
+
+    public void ReallyQuit()
+    {
+        Application.Quit();
+
+    }
+}
